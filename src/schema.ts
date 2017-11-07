7165f694c5c1724f372641574600cd72fe62890a
goulash1971 <goulash1971@gmail.com>
2017-11-08 08:56:50 +1100

    Checkin

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,13 @@
 import * as mongoose from 'mongoose';
 
-export const schema = (options: SchemaOptions) => (target: any, key: string) => {
+import { NoMetadataError } from './errors';
 
+export const schema = (options: SchemaOptions) => (target: any, key: string) => {
+  const Type = (Reflect as any).getMetadata('design:type', target, key);
+
+  if (!Type) {
+    throw new NoMetadataError(key);
+  }
 };
 
 export interface SchemaOptions extends mongoose.SchemaOptions {
